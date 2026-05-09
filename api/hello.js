f2a69306b1af99a88d0e69f14876958492f56d7a
Kavya <kavya@example.com>
2026-05-09 12:37:24 +0530

    Test: New hello endpoint to isolate API issues

---
+++ api/hello.js
@@ -0,0 +1,3 @@
+export default function handler(req, res) {
+  res.status(200).send('Hello from Vercel! If you see this, the API system is working.');
+}
--- api/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = (req, res) => {
-  res.status(200).json({ 
-    status: 'ok', 
-    message: 'Backend is ALIVE via CJS!',
-    env_check: process.env.DATABASE_URL ? 'DB_SET' : 'DB_MISSING'
-  });
-};
